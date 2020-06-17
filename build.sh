69af83e0e5ee38d6e5eb369d0fb2aa04fd5c1f93
Federico Madaffari <icomad@me.com>
2020-06-17 12:45:25 +0200

    added readme

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-yarn --cwd ./src/main/tailwind build:css && mvn clean:clean && mvn compiler:compile && mvn war:war
-rm -rf $CATALINA_HOME/webapps/image-gallery && rm $CATALINA_HOME/webapps/image-gallery.war
-cp ./target/image-gallery.war $CATALINA_HOME/webapps/
+yarn --cwd ./src/main/tailwind build:css && mvn clean:clean && mvn compiler:compile && mvn war:war && sudo rm -rf $CATALINA_HOME/webapps/image-gallery && sudo rm -f $CATALINA_HOME/webapps/image-gallery.war && sudo cp ./target/image-gallery.war $CATALINA_HOME/webapps/
